e6ea97631fb2681de1da5421df463be6cace113d
Vzudov94 <vzudov@samokat.ru>
2024-06-11 00:00:42 +0300

    commit 2

--- src/test/java/qa/qa/toolsDZTests.java
+++ src/test/java/qa/qa/toolsDZTests.java
@@ -27,21 +27,16 @@ public class toolsDZTests {
         $("#lastName").setValue("Zudov");
         $("#userEmail").setValue("vzudov@samokat.ru");
         $("#genterWrapper").$(byText("Male")).click();
-        //$("#gender-radio-1").click();
-        //getSelectedRadio(By.name("gender-radio-1"));
         $("#userNumber").setValue("89065058712");
-        //$("#dateOfBirthInput").click();
-        //$("[class=react-datepicker__month-select]").selectOption("March");
-        //$("[class=react-datepicker__year-select]").selectOption("1998");
-        //$("[class=react-datepicker__input-container]").$("[class=react-datepicker__day react-datepicker__day--012]").click();
-        //$(withText("react-datepicker__day--017")).click();
+        $("#dateOfBirthInput").click();
+        $("[class=react-datepicker__month-select]").selectOption("March");
+        $("[class=react-datepicker__year-select]").selectOption("1998");
+        $(".react-datepicker__day--021:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Commerce").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#currentAddress").setValue("Msk");
-        //$("#stateCity-wrapper").$(byText("Select State")).click();
-        //$("#stateCity-wrapper").$(byText("Select State")).setValue("Rajasthan").click();
-        //$("#stateCity-wrapper").$(byText("Select State")).$(byText("Rajasthan")).pressEnter();
-        //$("#stateCity-wrapper").$("[class=css-1uccc91-singleValue]").setValue("Agra").pressEnter();
+        $("#state input").setValue("Rajasthan").pressEnter();
+        $("#city input").setValue("Jaiselmer").pressEnter();
         $("#submit").click();
 
     }
