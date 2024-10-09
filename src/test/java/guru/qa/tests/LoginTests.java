d7ba96880a425ed890121e3a828275d3cf44aa1f
Vzudov94 <Vova.zudov.1994@main.ru>
2024-10-09 23:14:39 +0300

    commit hw14-7

--- src/test/java/guru/qa/tests/LoginTests.java
+++ src/test/java/guru/qa/tests/LoginTests.java
@@ -5,7 +5,6 @@ import guru.qa.pages.MainPage;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
